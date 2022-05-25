b86a0cc75c8a64ee25dff5b373eed353c9847022
gitjisu <kimvely0422@gmail.com>
2022-05-26 03:02:16 +0900

    0525

--- moviefront/src/views/BefView.vue
+++ moviefront/src/views/BefView.vue
@@ -38,10 +38,6 @@
 </template>
 
 <script>
-import $ from 'jquery'
-import TimelineMax from 'gsap';
-import Back from 'gsap'
-
 
 export default {
   name: 'BefView',
@@ -63,31 +59,6 @@ export default {
       window.scrollTo({left:0, top:0, behavior:"smooth"})
     }
   },
-  mounted() {
-      $(document).ready(function() {
-      $(".title").lettering();
-      $(".button").lettering();
-    });
-
-
-      $(document).ready(function() {
-        animation();
-      }, 1000);
-
-      $('.button').click(function() {
-        animation();
-      });
-
-
-    function animation() {
-      var title1 = new TimelineMax();
-      title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
-      title1.staggerFromTo(".title span", 0.5, 
-      {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
-      {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
-      title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
-    }
-  }
 }
 </script>
 
